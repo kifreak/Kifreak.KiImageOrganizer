43229f686bd09ebc15913d055544393d306da0aa
Kifreak <developer@kifreak.com>
2019-04-25 09:11:27 +0200

    Add tests for Separated By Commas formatter

--- Kifreak.KiImageOrganizer.Tests/Formatters/FormatterTest.cs
+++ Kifreak.KiImageOrganizer.Tests/Formatters/FormatterTest.cs
@@ -45,5 +45,27 @@ namespace Kifreak.KiImageOrganizer.Tests.Formatters
             result = formatters.Format(null, null);
             Assert.AreEqual(@"\", result);
         }
+
+        [TestMethod]
+        public void SeparatedByCommasOk()
+        {
+            SeparatedByCommasFormatters formatters = new SeparatedByCommasFormatters();
+            var result = formatters.Format("key", "value");
+            Assert.AreEqual("key,value", result);
+        }
+
+        [TestMethod]
+        public void SeparatedByCommasKo()
+        {
+            SeparatedByCommasFormatters formatters = new SeparatedByCommasFormatters();
+            var resultNoKey = formatters.Format(string.Empty, "value");
+            var resultNoValue = formatters.Format("key", string.Empty);
+            var resultNoData = formatters.Format(string.Empty, string.Empty);
+            var resultNull = formatters.Format(null, null);
+            Assert.AreEqual("value", resultNoKey);
+            Assert.AreEqual("key", resultNoValue);
+            Assert.AreEqual("", resultNoData);
+            Assert.AreEqual("", resultNull);
+        }
     }
 }
